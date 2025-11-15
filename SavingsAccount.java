309c9ee43b4c713b2e15144a9ad89c1ee82f476a
Paul <Paul.Manicio@robertson.brightspace.com>
2025-11-15 16:53:55 -0600

    Constructors and Accessors Added

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -4,4 +4,17 @@ public class SavingsAccount {
     private final int ACCOUNT_NUMBER;
 
     private double balance;
+
+    public SavingsAccount(int accountNumber, double initialBalance) {
+        this.ACCOUNT_NUMBER = accountNumber;
+        this.balance = startingBalance;
+    }
+
+    public int getAccountNumber() {
+        return ACCOUNT_NUMBER;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
