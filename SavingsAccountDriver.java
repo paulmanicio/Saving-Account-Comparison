c747fe7ee751410a42a6cc9f498f588d7fc28012
Paul <Paul.Manicio@robertson.brightspace.com>
2025-11-15 19:17:02 -0600

    Loop and Sum of the accounts added

--- SavingsAccountDriver.java
+++ SavingsAccountDriver.java
@@ -7,7 +7,32 @@ public class SavingsAccountDriver {
 
         System.out.println("Monthly balances for one year with 5% annual interest: ");
         System.out.println();
-        System.out.println("Month   Account#   Balance   Account#   Balance");
-        System.out.println("_______________________________________________");
+        System.out.println("Month  Account#    Balance   Account#     Balance");
+        System.out.println("_________________________________________________");
+
+        for(int month = 0; month < 12; month++) {
+            if(month == 0) {
+                System.out.printf("%-6d %-9d %9.2f    %-9d %9.2f%n", 0,
+                saver1.getAccountNumber(),
+                saver1.getBalance(),
+                
+                saver2.getAccountNumber(),
+                saver2.getBalance());
+            } else {
+                saver1.addMonthlyInterest();
+                saver2.addMonthlyInterest();
+
+                System.out.printf("%-6d %-9d %9.2f    %-9d %9.2f%n", 
+                month,
+                saver1.getAccountNumber(),
+                saver1.getBalance(),
+                
+                saver2.getAccountNumber(),
+                saver2.getBalance());
+            }
+        }
+        double total = saver1.getBalance() + saver2.getBalance();
+        System.out.println();
+        System.out.println("Final Balance (Accounts combined): $ " + String.format("%.2f", total));
     }
 }
